23a77fabcbd95b5071a6441b6ad0526b0a3c9f31
a01092505550-bit <a01092505550@gmail.com>
2025-10-12 10:44:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,5 @@ def home():
     return "AutoFit Flask Server is Running! ✅"
 
 if __name__ == "__main__":
-    # Render 환경에서는 반드시 0.0.0.0 + PORT 환경변수를 사용해야 함
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
-
